b549a82459eddb5eabfa26493e8b68192e082307
louiszawadzki <louis.zawadzki@datadoghq.com>
2024-01-19 17:59:21 +0100

    Add iOS tests

--- packages/core/ios/Sources/DatadogSDKWrapper.swift
+++ packages/core/ios/Sources/DatadogSDKWrapper.swift
@@ -28,10 +28,10 @@ public class DatadogSDKWrapper {
     private init() {}
 
     // Initialization callbacks
-    private var onOnCoreInitializedListeners: [OnCoreInitializedListener] = []
+    internal var onCoreInitializedListeners: [OnCoreInitializedListener] = []
     
     public func addOnCoreInitializedListener(listener:@escaping OnCoreInitializedListener) {
-        onOnCoreInitializedListeners.append(listener)
+        onCoreInitializedListeners.append(listener)
     }
 
     // Core instance
@@ -54,7 +54,7 @@ public class DatadogSDKWrapper {
     ) -> Void {
         let core = Datadog.initialize(with: configuration, trackingConsent: trackingConsent)
         setCoreInstance(core: core)
-        for listener in onOnCoreInitializedListeners {
+        for listener in onCoreInitializedListeners {
             listener(core)
         }
     }
